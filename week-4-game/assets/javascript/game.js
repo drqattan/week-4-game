d6c2b4eba10e3f542b45680574aee0701f0948f4
Marame Gattan <drqattan@hotmail.com>
2017-03-15 12:10:35 -0400

    final submission for homework with no restart

--- week-4-game/assets/javascript/game.js
+++ week-4-game/assets/javascript/game.js
@@ -1,110 +1,126 @@
 
-	//give a random number =<35
-	// assign it to the target number
-	// pull the picture ids 
-	//assign random numbers to the picture ids <20
-	//add the numbers together everytime you press a crystal 
-	//if the number together is higher than the expected number then you loose if its equal then you win. 
-	//return back to the number of wins board
+	
+	//create a foreloop for all the stones (multiple attempts have failed.)
+	//restart the game once declared win or loss (multiple attempts have failed.)
 
-	//create a foreloop for all the stones
+//declaring the variables from 1-50.	
 var number = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50];
 
-// var randomnumber = function randomnumber(1,50) {
-// 	return Math.floor((Math.random()*50) +1);
-// 	console.log(randomnumber)
-// }
-
-// $("#randomNumber").ready(function) {
-
-	//The Value of Random Number generated via randomization
+//The target number which is a number between 1 and 50
 var rand = number[Math.floor(Math.random() * number.length)];
 var randomnumber = Math.floor(Math.random()*50)
 $("#randomNumber").append(randomnumber);
 console.log(randomnumber);
-// }
+
+//The score that you generate. 
+var yourscore = 0
 
 
-	//The value of stone 1
-var stone1 = Math.floor(Math.random()*35);
-$("#Stone1").click(function(){
-	$("#yourScore").html(function(i,val) {return + val + stone1});
+		//The value of stones[i]
+		var stone1 = Math.floor(Math.random()*35);
+		$("#Stone1").click(function(){
+		$("#yourScore").append(function() {
+		yourscore += stone1   //creating a total for your number
+		$("#yourScore").html(function(i,val) {return + val + stone1});//reporting total your number to total score
 		});
-	console.log(stone1)
-	// $("#yourScore").append(function() {
-	// 	yourScore++ stone1
-
+		});
+		console.log(stone1)
 	
-var stone2 = Math.floor(Math.random()*35);
-$("#Stone2").click(function(){
-	$("#yourScore").html(function(i,val) {return + val + stone2});
+		var stone2 = Math.floor(Math.random()*35);
+		$("#Stone2").click(function(){
+		$("#yourScore").append(function() {
+		yourscore += stone2
+		$("#yourScore").html(function(i,val) {return + val + stone2});
 		});
-	console.log(stone2)
-
-
-var stone3 = Math.floor(Math.random()*35);
-$("#Stone3").click(function(){
-	$("#yourScore").html(function(i,val) {return + val + stone3});
 		});
-	console.log(stone3)
+		console.log(stone2)
 
-var stone4 = Math.floor(Math.random()*35);
-$("#Stone4").click(function(){
-	$("#yourScore").html(function(i,val) {return + val + stone4});
+		var stone3 = Math.floor(Math.random()*35);
+		$("#Stone3").click(function(){
+		$("#yourScore").append(function() {
+		yourscore += stone3
+		$("#yourScore").html(function(i,val) {return + val + stone3});
 		});
-	console.log(stone4)
-
-var stone5 = Math.floor(Math.random()*35);
-$("#Stone5").click(function(){
-	$("#yourScore").html(function(i,val) {return + val + stone5});
 		});
-	console.log(stone5)
+		console.log(stone3)
 
-var stone6 = Math.floor(Math.random()*35);
-$("#Stone6").click(function(){
-	$("#yourScore").html(function(i,val) {return + val + stone6});
+		var stone4 = Math.floor(Math.random()*35);
+		$("#Stone4").click(function(){
+		$("#yourScore").append(function() {
+		yourscore += stone4
+		$("#yourScore").html(function(i,val) {return + val + stone4});
 		});
-	console.log(stone6)	
+		});
+		console.log(stone4)
 
-// $("#yourScore") === var yourscore 
-// $("#randomNumber") === var randomnumber
+		var stone5 = Math.floor(Math.random()*35);
+		$("#Stone5").click(function(){
+		$("#yourScore").append(function() {
+		yourscore += stone5
+		$("#yourScore").html(function(i,val) {return + val + stone5});
+		});
+		});
+		console.log(stone5)
 
-$("#Stones").click(function() {
-if ("#yourScore" === "#randomNumber") {
-	alert ("You Win")
-	$("#wins").html(function(i,val) {return + val + 1});
-}
+		var stone6 = Math.floor(Math.random()*35);
+		$("#Stone6").click(function(){
+		$("#yourScore").append(function() {
+		yourscore += stone6
+		$("#yourScore").html(function(i,val) {return + val + stone6});
+		});
+		});
+		console.log(stone6)	
 
+//tried to create one code for multiple stones but didnt work
+// for (var i=0;i<7;i++)
+// var stone[i] = Math.floor(Math.random()*35);
+// $("#Stone[i]").click(function(){
+// $("#yourScore").append(function() {
+// yourscore += stone[i]
+// $("#yourScore").html(function(i,val) {return + val + stone[i]});
+// });
+// });
 
-if ("#yourScore" > "#randomNumber") {
-	alert ("you Lose")
-	$("#loss").html(function(i,val) {return + val + 1});
-}
+		//this is the logic behin the win and loss situation.
+	$("#Stones").click(function() {
+		console.log(yourscore)
 
-function ResetGlobalVariables () {
-	var randomnumber = Math.floor(Math.random()*50);
-	$("#yourScore").append(0);
+		if (yourscore > randomnumber) {
+		$("#loss").html(function(i,val) {return + val + 1});//reporting back as a loss
+		alert ("you Lose") 
+		}
 
-}
- // clear : function() {
- // 	randomNumber & $("#yourScore" = 0)
-
- //  }
+		if (yourscore === randomnumber) {
+		$("#wins").html(function(i,val) {return + val + 1});//reporting back as a win
+		alert ("You Win")
+		}
 	})
 
-// var Stone1 = Math.floor(Math.random()*35);
-// function randomWholeNum () {
-// 	return Math.stone1
-// 	$("#stone1"). on("click", function()
-	
+//multiple attempts to restart the game 
 
-// $(document).ready(function() {
+	// function	Gameover {
+	// 	ctx.canvas.removeEventListener('alert', function(event){
+	// 	canvasMouseOver(event);
+	// 	})
+	// ResetGlobalVariables();
+	// }
 
-//       // When random-button is clicked...
-//       $("#crystalOne").on("click", function() {
 
-//         // ... we generate a random number
-//         var random = Math.floor(Math.random() * 35) + 1;
+	// function ResetGlobalVariables () {
+	// 	var randomnumber = Math.floor(Math.random()*50);
+	// 	$("#yourScore").append(0);
+	// }
+ 
 
-//         // ... and then dump the random number into our random-number div.
-//         $("your score").html("yourscore" + "crystalOne");
+ 	// clear : function() {
+ 	// 	randomNumber & $("#yourScore" = 0)
+	//  }
+
+	// $("#newGame").click(function(){
+	// 	var rand = number[Math.floor(Math.random() * number.length)];
+	// 	var randomnumber = Math.floor(Math.random()*50)
+	// $("#randomNumber").append(randomnumber);
+	// console.log(randomnumber);
+	// // }
+	// var yourscore = 0
+	// 	}
